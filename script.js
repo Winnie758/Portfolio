9cf735051b4a71148617bd2c5e597d43de917d6c
Winnie758 <mwethyamutisya.22@gmail.com>
2026-03-31 14:11:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -32,10 +32,10 @@ function myMenuFunction(){
   
   /* ----- TYPING EFFECT ----- */
   let typingEffect = new Typed(".typedText",{
-    strings : ["Machine Learning Developer","Mathematical Modeling Specialist","Web Designer","Data Analysis Expert"],
+    strings : ["Machine Learning Developer","Mathematical Modeling Specialist","Web Developer","Data Analyst"],
     loop : true,
     typeSpeed : 100,
-    backSpeed : 50,
+    backSpeed : 40,
     backDelay : 200
   })
   
